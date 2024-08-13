dcd87f9b36b4cbaf53151d3afc52b3ee97f308a7
hogretup <e0942306@u.nus.edu>
2024-08-13 10:22:14 +0800

    add preview

--- src/components/blogs/BlogForm.tsx
+++ src/components/blogs/BlogForm.tsx
@@ -24,7 +24,6 @@ const BlogForm = () => {
     });
   };
 
-  //can just make a github webhook to run pipeline if succesful
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     console.log("Form values:", fields);
@@ -34,53 +33,77 @@ const BlogForm = () => {
       createItem("posts", {
         title: fields.title,
         author: {
-            name: fields.author
+          name: fields.author,
         },
         content: fields.content,
         published_date: currentDate,
         slug: fields.slug,
+        image: fields.image,
       })
     );
-    
-    console.log(result)
+
+    console.log(result);
   };
 
   return (
-    <Form style={{ padding: "30px" }} onSubmit={handleSubmit}>
-      <BlogFormInput
-        title="slug"
-        placeHolder="to be used in url"
-        value={fields.slug}
-        onChange={handleChange}
-      />
-      <BlogFormInput
-        title="title"
-        placeHolder="title of blog"
-        value={fields.title}
-        onChange={handleChange}
-      />
-      <BlogFormInput
-        title="content"
-        placeHolder="write your paragraphs here"
-        value={fields.content}
-        onChange={handleChange}
-      />
-      <BlogFormInput
-        title="image"
-        placeHolder="put URL??"
-        value={fields.image}
-        onChange={handleChange}
-      />
-      <BlogFormInput
-        title="author"
-        placeHolder="name of author"
-        value={fields.author}
-        onChange={handleChange}
-      />
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <div style={{ display: "flex" }}>
+      <Form style={{ padding: "30px", width: "50%" }} onSubmit={handleSubmit}>
+        <BlogFormInput
+          title="slug"
+          placeHolder="to be used in url"
+          value={fields.slug}
+          onChange={handleChange}
+        />
+        <BlogFormInput
+          title="title"
+          placeHolder="title of blog"
+          value={fields.title}
+          onChange={handleChange}
+        />
+        <BlogFormInput
+          title="content"
+          placeHolder="write your paragraphs here"
+          value={fields.content}
+          onChange={handleChange}
+        />
+        <BlogFormInput
+          title="image"
+          placeHolder="put URL??"
+          value={fields.image}
+          onChange={handleChange}
+        />
+        <BlogFormInput
+          title="author"
+          placeHolder="name of author"
+          value={fields.author}
+          onChange={handleChange}
+        />
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+      <div style={{ padding: "30px", width: "50%" }}>
+        <main>
+          {fields.image ? (
+            <img
+              src={`YOUR_DIRECTUS_URL/assets/${fields.image}?width=500`}
+              alt="Preview"
+            />
+          ) : (
+            <div style={{ height: "250px", background: "#ccc" }}>
+              Image Preview
+            </div>
+          )}
+          <h1>{fields.title || "Blog Title"}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: fields.content || "Your content will appear here...",
+            }}
+          />
+        </main>
+      </div>
+    </div>
   );
 };
 
